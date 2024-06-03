0a7e84f99dd49e12d32866e20130950faf07f525
vinod5025 <vinodthombare752@gmail.com>
2024-06-04 00:10:17 +0530

    port

--- conn.js
+++ conn.js
@@ -1,19 +1,11 @@
-const mysql = require('mysql');
-const util = require('util');
-const dotenv = require('dotenv');
-dotenv.config();
-var conn = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+var mysql=require("mysql");
+var util=require("util");
+var conn=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"smartbazar"
+
 });
-conn.connect(function (error) {
-    if (error) {
-        throw error;
-    } else {
-        console.log('MySQL Database is connected successfully');
-    }
-});
-var exe = util.promisify(conn.query).bind(conn);
-module.exports = exe;
+var exe=util.promisify(conn.query).bind(conn);
+module.exports=exe;
--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var dotenv = require('dotenv');
-var upload = require("express-fileupload");
-var session = require("express-session");
-var user_route = require("./routes/user_routes");
-var admin_route = require("./routes/admin_routes");
-var business_route = require("./routes/business_routes");
-const path = require('path');
-dotenv.config();
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+var express=require("express");
+var bodyparser=require("body-parser");
+var upload=require("express-fileupload");
+var session=require("express-session");
+var user_route= require("./routes/user_routes");
+var admin_route=require("./routes/admin_routes");
+var business_route=require("./routes/business_routes");
+var app=express();
+app.use(bodyparser.urlencoded({extended:true}));
 app.use(upload());
 app.use(session({
-    secret: "23456",
-    resave: true,
-    saveUninitialized: true
+    secret:"123",
+    resave:true,
+    saveUninitialized:true
+
 }));
 app.use(express.static("public/"));
-app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
+app.use("/admin",admin_route)
+app.use("/business",business_route)
+app.use("/",user_route);
 
-app.use("/", user_route);
-app.use("/admin", admin_route);
-app.use("/business", business_route);
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
-// Start the server
-const PORT = process.env.PORT || 2000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+app.listen(1000);
+
